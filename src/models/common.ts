5d3683e07dfb9dad94b5bff66da523ded65b9cd7
Fabricio Murillo Flores <fabriciomf72@gmail.com>
2023-08-23 22:16:15 -0600

    add common model

---
+++ src/models/common.ts
@@ -0,0 +1,13 @@
+export enum StatusCodes {
+    NotFound = 404,
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    InternalError = 500
+}
+
+export enum ResponseStatus {
+    Success = 'success',
+    Error = 'error'
+}
--- src/models/task.ts
+++ src/models/task.ts
@@ -7,16 +7,3 @@ export interface Tasks {
 
 export const STATUS_COMPLETED = 'completada'
 export const STATUS_PENDING = 'pendiente'
-export enum StatusCodes {
-    NotFound = 404,
-    Success = 200,
-    Created = 201,
-    Accepted = 202,
-    BadRequest = 400,
-    InternalError = 500
-}
-
-export enum ResponseStatus {
-    Success = 'success',
-    Error = 'error'
-}
